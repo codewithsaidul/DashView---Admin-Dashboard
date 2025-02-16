fc4f7cd3e3210d4db5858ee15c67c6389cc88aa8
codewithsaidul <codewithsaidul@gmail.com>
2025-02-16 15:42:28 +0600

    Layout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import Products from "./pages/Products/Products"
 import Users from "./pages/users/Users"
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
+import Menu from "./components/Menu/Menu"
 
 function App() {
 
   const Layout = () => {
     return (
-      <div className="main"></div>
+      
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menu__container">
+            <Menu />
+          </div>
+          <div className="content__container">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
     )
   }
 
@@ -15,15 +30,21 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />
-    },
-    {
-      path: "/users",
-      element: <Users />
-    },
-    {
-      path: "/products",
-      element: <Products />
+      element: <Layout />, 
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/users",
+          element: <Users />
+        },
+        {
+          path: "/products",
+          element: <Products />
+        }
+      ]
     }
   ])
 
